fe123a65d40d0fc95b029d6480a6d8ad28855b87
TiDaBo <illinois2007@gmail.com>
2025-07-15 04:33:29 +0300

    First commit

--- homework/5_hw.py
+++ homework/5_hw.py
@@ -10,26 +10,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome() #./chromedriver
-driver.get("https://www.saucedemo.com/")
+def find_saucedemo_elements():
 
+    driver = webdriver.Chrome() #./chromedriver
+    driver.get("https://www.saucedemo.com/")
 
-text_field_username = driver.find_element(By.CSS_SELECTOR, '#user-name')
-if text_field_username is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
+    text_field_username = driver.find_elements(By.CSS_SELECTOR, '#user-name')
+    text_field_pass = driver.find_elements(By.CSS_SELECTOR, '#password')
+    submit_button = driver.find_elements(By.CSS_SELECTOR, '#login-button')
 
-
-text_field_pass = driver.find_element(By.CSS_SELECTOR, '#password')
-if text_field_pass is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
-
-
-submit_button = driver.find_element(By.CSS_SELECTOR, '*.submit-button')
-if text_field_pass is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
+    if text_field_username and text_field_pass and submit_button:
+        print('Элементы найдены')
+    else:
+        print('Элементы не найдены')
